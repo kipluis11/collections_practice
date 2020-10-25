4dafd2bc71e4f5f9b6f66347acfe607647850639
kip_parcell <kipluis@gmail.com>
2020-10-24 23:44:10 -0700

    commit tree 246eb3b1ae499bbed3ae258a6c521209d437cb24

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
 
+up = [25, 7, 1]
+def sort_array_asc(up)
+    up.sort
+end
+
+def sort_array_desc(down)
+    down.sort.reverse
+end
+
+def sort_array_char_count(long)
+    long.sort {|l, r| l.length <=> r.length}
+end
+
+def swap_elements(swap)
+    swap[0], swap[1], swap[2] = swap[0], swap[2], swap[1]
+end
+
+def reverse_array(rev)
+    rev.reverse
+end
+
+def kesha_maker(jack)
+    jack.each {|k| k[2] = "$"}
+    return jack
+end
+
+def find_a(a)
+    a.select {|word| word.start_with?('a')}
+end
+
+def sum_array(add)
+    add.inject(0) {|sum, i| sum + i}
+end
+
+def add_s(lisp)
+    lisp.each_with_index.collect do |element, index|
+        if index == 1
+            element
+        else element[element.length] = "s"
+            element
+        end
+    end
+end
+    
